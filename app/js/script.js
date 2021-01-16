8994c582a7be625c2cc0b17102cf0a5b2d90e032
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-01-16 20:22:01 +0300

    Initial commit. Finished navbar, hamburger menu and mobile menu along with animations. Halfway through hero section.

---
+++ app/js/script.js
@@ -0,0 +1,23 @@
+console.log("js connected");
+
+const body = document.querySelector("body");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+// const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    //close hamburger menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((el) => el.classList.remove("fade-in"));
+    fadeElems.forEach((el) => el.classList.add("fade-out"));
+  } else {
+    // open hamburger menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((el) => el.classList.remove("fade-out"));
+    fadeElems.forEach((el) => el.classList.add("fade-in"));
+  }
+});
